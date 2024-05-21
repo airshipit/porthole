80f3dfd08ded28656239553cfd68f6a46f023323
astebenkova <astebenkova@mirantis.com>
2024-05-21 12:36:39 +0300

    Improve 004-make-images script
    Enhance the script to build local images by passing the
    appropriate environment variables supported by Makefile
    
    Change-Id: Ibab99b725cad77891064105d37b46105eb8fd747

--- tools/deployment/004-make-images.sh
+++ tools/deployment/004-make-images.sh
@@ -17,25 +17,11 @@ set -x
 : "${DISTRO:="ubuntu_focal"}"
 
 env
-make images
 
+echo "[*] Deploy local docker registry..."
 docker rm registry --force || true
 docker run -d -p 5000:5000 --restart=always --name registry registry:2
 
-docker tag quay.io/airshipit/porthole-calicoctl-utility:latest-${DISTRO} localhost:5000/porthole-calicoctl-utility:latest-${DISTRO}
-docker tag quay.io/airshipit/porthole-ceph-utility:latest-${DISTRO} localhost:5000/porthole-ceph-utility:latest-${DISTRO}
-docker tag quay.io/airshipit/porthole-compute-utility:latest-${DISTRO} localhost:5000/porthole-compute-utility:latest-${DISTRO}
-docker tag quay.io/airshipit/porthole-etcdctl-utility:latest-${DISTRO} localhost:5000/porthole-etcdctl-utility:latest-${DISTRO}
-docker tag quay.io/airshipit/porthole-mysqlclient-utility:latest-${DISTRO} localhost:5000/porthole-mysqlclient-utility:latest-${DISTRO}
-docker tag quay.io/airshipit/porthole-openstack-utility:latest-${DISTRO} localhost:5000/porthole-openstack-utility:latest-${DISTRO}
-docker tag quay.io/airshipit/porthole-postgresql-utility:latest-${DISTRO} localhost:5000/porthole-postgresql-utility:latest-${DISTRO}
+echo "[*] Build images and push them to local docker registry..."
+make images -e DOCKER_REGISTRY="localhost:5000" -e PUSH_IMAGE=true
 
-
-
-docker push localhost:5000/porthole-calicoctl-utility:latest-${DISTRO}
-docker push localhost:5000/porthole-ceph-utility:latest-${DISTRO}
-docker push localhost:5000/porthole-compute-utility:latest-${DISTRO}
-docker push localhost:5000/porthole-etcdctl-utility:latest-${DISTRO}
-docker push localhost:5000/porthole-mysqlclient-utility:latest-${DISTRO}
-docker push localhost:5000/porthole-openstack-utility:latest-${DISTRO}
-docker push localhost:5000/porthole-postgresql-utility:latest-${DISTRO}
